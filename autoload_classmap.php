8d1ffcea659cb002d909de9e00c8a49916d23bf0
Wesley Overdijk <r.w.overdijk@gmail.com>
2012-07-29 00:55:30 +0200

    fix tabs

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -1,5 +1,6 @@
 <?php
+
 return array(
-    'SxRequireJs\Module'                        => __DIR__ . '/Module.php',
-    'SxRequireJs\View\Helper\SxRequireJs'		=> __DIR__ . '/src/SxRequireJs/View/Helper/SxRequireJs.php'
+    'SxRequireJs\Module'                  => __DIR__ . '/Module.php',
+    'SxRequireJs\View\Helper\SxRequireJs' => __DIR__ . '/src/SxRequireJs/View/Helper/SxRequireJs.php'
 );
