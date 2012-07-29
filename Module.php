3b2d3853096ebd913d72282e9400748949e6148f
Wesley Overdijk <r.w.overdijk@gmail.com>
2012-07-29 02:09:41 +0200

    Removed onBootstrap
    Remainder of moduleskeleton removed (not needed, not module
    responsibility)

--- Module.php
+++ Module.php
@@ -2,7 +2,6 @@
 namespace SxRequireJs;
 
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-use Zend\Mvc\ModuleRouteListener;
 
 class Module implements AutoloaderProviderInterface
 {
@@ -23,14 +22,5 @@ class Module implements AutoloaderProviderInterface
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function onBootstrap($e)
-    {
-        // You may not need to do this if you're doing it elsewhere in your
-        // application
-        $eventManager        = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
     }
 }
