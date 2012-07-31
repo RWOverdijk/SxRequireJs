b0756e41e35676511eeb5ae9dd402c688b593fc7
Wesley Overdijk <r.w.overdijk@gmail.com>
2012-07-31 15:07:08 +0300

    Update config/module.config.php
    Replaced tabs with spaces

--- config/module.config.php
+++ config/module.config.php
@@ -2,12 +2,12 @@
 return array (
     'view_manager' => array(
         'template_path_stack' => array(
-            'sxrequirejs' 	=> __DIR__ . '/../view',
+            'sxrequirejs' => __DIR__ . '/../view',
         ),
     ),
-	'view_helpers' => array (
-		'invokables' => array (
-			'requirejs' => 'SxRequireJs\View\Helper\SxRequireJs',
-		)
-	)
+    'view_helpers' => array (
+        'invokables' => array (
+            'requirejs' => 'SxRequireJs\View\Helper\SxRequireJs',
+        )
+    )
 );
